b0290b2f36876c70038853aefeac21ced4db91e5
Chprasad <cpbehera03@gmail.com>
2023-04-06 09:38:41 +0530

    changed functionality

--- script.js
+++ script.js
@@ -3,42 +3,55 @@ let timerHeading = document.getElementById('timerHeading');
 let timer = document.getElementById('timer');
 let slides = document.getElementsByClassName('slides');
 
+slides[0].style.display = "block";
+
 let counter = 10;
 
 timer.textContent = counter;
 
 
-setInterval(countDownTimer,1000);
+setInterval(countDownTimer, 1000);
 
-function countDownTimer(){
+function countDownTimer() {
     counter--;
     timer.textContent = counter;
-    if(counter == 0){
-        counter = 0;
-        timerHeading.style.display = "none";
-    }
-
-}
-setTimeout(showDiv,10000);
-
-
-function showDiv(){
-    imageGallery.style.display = "block";
-    slides[0].style.display = "block";
-    setInterval(changeImg,5000);
-}
-
-let count = 1;
-
-function changeImg(){
-    if(slides[count] < slides.length){
-        slides[count].style.display = "block";
-        console.log(slides[count]);
-        count++;
-        if(slides[count] == slides.length){
-            count = 0;
-        }
+    if (counter == 0) {
+        counter = 6;
 
     }
 
 }
+let count = slides.length - 1;
+
+setTimeout(changeFirstImg, 10000);
+// setInterval(changeImg, 5000);
+
+
+function changeFirstImg() {
+    slides[count].style.opacity = 0;
+    console.log(slides[count]);
+    console.log(count);
+    count = count - 1;
+    console.log(count);
+}
+
+// if (setTimeout(changeFirstImg, 10000)) {
+//     setInterval(() => {
+//         if (slides[count] >= 0) {
+//             slides[count].style.opacity = 0;
+//             // console.log(slides[count], count);
+//         }
+//     }, 5000);
+//     count--;
+// } else {
+//     count = slides.length - 1;
+// }
+
+
+
+
+
+// function changeImg() {
+
+
+// }
